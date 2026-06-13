11b4fa01ca85e73de0d527dbc5836f069eb46790
robsonclaser-dev <robsonclaser@gmail.com>
2026-06-13 19:06:16 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,20 @@
-const CACHE_NAME = '2t-producao-v1';
-const ASSETS = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
+const CACHE_NAME = '2t-prod-v4';
+const ASSETS = ['/PRODU-AO-2T/', '/PRODU-AO-2T/index.html', '/PRODU-AO-2T/manifest.json'];
 
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS).catch(()=>{})));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
   self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/PRODU-AO-2T/index.html')))
   );
 });
